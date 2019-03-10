3710113af54cb51822859e829efa2d09c474099f
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-03-10 04:21:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,8 @@ def find_the_cheese (list)
   cheese_types = ["cheddar", "gouda", "camembert"]
   list.any? do |x|
     cheese_types.include?(x)
+  if list.include?(cheddar) == true 
+    return "cheddar"
   end
   if cheese_types.include?(list) == false
     return nil
