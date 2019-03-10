a52fc6fb294fe60c4ae36bca2db5a51b3642ba9f
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-03-10 04:24:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 
 def find_the_cheese (list)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheddar = cheese_type.shift
+  cheddar = cheese_types.shift
   list.any? do |x|
     cheese_types.include?(x)
   end 
